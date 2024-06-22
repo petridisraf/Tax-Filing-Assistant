f164a2dd7cde7e0dc2e1b704297bf827b2e71ade
petridisraf <146370490+petridisraf@users.noreply.github.com>
2024-06-22 18:33:15 +0300

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tax_filing_assistant.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['OPENAI_API_KEY'] = 'fill OPENAI_API_KEY'  
+    app.config['OPENAI_API_KEY'] = 'sk-proj-zEF5cRDrB2jYNGPbs2DHT3BlbkFJ7b47wSMSLKaSxKuUs6jq'  
 
     db.init_app(app)
     migrate.init_app(app, db)
@@ -18,5 +18,5 @@ def create_app():
     app.register_blueprint(main)
 
     return app
-
+# sk-proj-zEF5cRDrB2jYNGPbs2DHT3BlbkFJ7b47wSMSLKaSxKuUs6jq
 
